063b0d6617639b5df9632e9522c871009acf8216
LeoLin6 <61672672+LeoLin6@users.noreply.github.com>
2024-04-05 18:31:05 -0500

    Add files via upload

--- calibration_func_turretbot.ino
+++ calibration_func_turretbot.ino
@@ -1,12 +1,17 @@
 #include <AccelStepper.h>
 const int Pin=8; //for induction sensor
-AccelStepper stepper1(1, 3, 6); 
+const int pitch_pin = 11; //pitch induction sensor
+AccelStepper stepper1(1, 2, 5);
+AccelStepper pitch_motor(1,3,6); 
 
 void setup() {
     pinMode(Pin, INPUT);
+    pinMode(pitch_pin, INPUT);
     //Serial.begin(384000);
     stepper1.setMaxSpeed(2000);
     stepper1.setSpeed(2000); 
+    pitch_motor.setMaxSpeed(100000);
+    pitch_motor.setSpeed(200); 
 }
  
 void loop() {
@@ -14,6 +19,7 @@ void loop() {
     //stepper1.runSpeed();
 
     run_calibration_seq();
+    pitch_calibration();
     
 }
 
@@ -25,6 +31,16 @@ void run_calibration_seq(){
     
     Serial.println("Object detected");
     stepper1.stop();
+}
+
+void pitch_calibration(){
+    while(digitalRead(pitch_pin)!=HIGH){
+      pitch_motor.runSpeed();
+      Serial.println("no object");
+    }
+    
+    Serial.println("Object detected");
+    pitch_motor.stop();
 }
 
 void stop() {
