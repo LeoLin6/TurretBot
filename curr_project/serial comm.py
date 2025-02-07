bf856a21114bc53676bfa906e5746d64239b42bc
LeoLin6 <61672672+LeoLin6@users.noreply.github.com>
2025-02-07 16:56:38 -0600

    Add files via upload

--- curr_project/serial comm.py
+++ curr_project/serial comm.py
@@ -2,12 +2,22 @@
 import serial 
 import time 
 arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1) 
+'''
 def write_read(x): 
-	   arduino.write(bytes(x, 'utf-8')) 
-	   time.sleep(0.05) 
-	   data = arduino.readline() 
-	   return data 
+	arduino.write(bytes(x, 'utf-8')) 
+	time.sleep(0.05) 
+	data = arduino.readline() 
+	return data 
+'''
 while True: 
-	   num = input("Enter the position you want to move to(degrees): ") # Taking input from user 
-	   value = write_read(num) 
-	   print(value) # printing the value 
+        string = input("Enter 1 for calibration, 2 for positional movement: ")
+        if string =='1':
+            arduino.write(b"cali\n")
+        elif string=='2':
+            num = input("Enter the position you want to move to(degrees): ") # Taking input from user
+            arduino.write(b"pos\n")
+            arduino.write(bytes(num, 'utf-8'))
+
+        #print(value) # printing the value
+                                   
+    
